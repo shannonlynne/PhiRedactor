1b313efd354d95d3f31f7d1e420d379a4b2d7038
Shannon Morrell <shannon.morrell2@gmail.com>
2025-07-28 00:36:03 +0000

    some updates

--- frontend/src/services/redactionPost.tsx
+++ frontend/src/services/redactionPost.tsx
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 const apiUrl = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
-});
+    // baseURL: process.env.REACT_APP_API_BASE_URL,
+    baseURL: "https://glorious-train-7v7w9jxjjgjfpq5j-5000.app.github.dev"
+})
 
 export default async function redactionPost(files: File[]) {
   const formData = new FormData();
   files.forEach((file) => formData.append("formData", file));
+
+  console.log("API base URL:", process.env.REACT_APP_API_BASE_URL);
 
   try {
     const response = await apiUrl.post("/api/redaction/text/redact", formData, {
